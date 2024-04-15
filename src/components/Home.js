1de00bae0460e89ad834d3f577f5c2a4de3eeee8
Mayson_santos <maysonsantos46@gmail.com>
2024-04-15 18:04:54 -0300

    resolver bug de caminho de img

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,13 +15,13 @@ return (
                 um apaixonado desenvolvedor front-end com uma sede insaciável de criar experiências digitais memoráveis e funcionais. Meu percurso começa com minha formação em Análise e Desenvolvimento de Sistemas, uma jornada que solidificou minha compreensão dos pilares da computação e da lógica de programação. No entanto, é no desenvolvimento front-end que encontrei minha verdadeira vocação.
                 Focando inteiramente no front-end, mergulhei de cabeça no universo do HTML, CSS e JavaScript, explorando cada linha de código . Cada projeto é uma oportunidade de dar vida a conceitos abstratos, transformando-os em interfaces intuitivas e visualmente deslumbrantes.</div>
             
-            <a href="/democv.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
+            <a href="democv.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
                 Download Meu CV
             </a>
         </div>
         <div className="avatar">
             <div className="card">
-                <img src="public/avatar.png" alt="" />
+                <img src='public/avatar.png' alt="" />
                 
             </div>
         </div>
