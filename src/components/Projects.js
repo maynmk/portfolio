1800841256102432ab98b77b54b9a396e0b104f8
Mayson_santos <131605506+maynmk@users.noreply.github.com>
2024-11-18 12:26:24 -0300

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -44,6 +44,14 @@ const [listProjects] = useState([
     language: 'WordPress,Elementor,Php',
     images: 'drop.png',
     link: 'https://lojacloud.com.br/'
+  },
+  {
+    name: 'CLASSIFICADOS MÉDICOS BRASIL',
+    des: 'Essa plataforma de marketplace foi projetada com cuidado para conectar compradores e vendedores de maneira segura e eficiente. Com uma interface intuitiva e recursos práticos, é possível anunciar produtos e serviços com facilidade, maximizando suas oportunidades de negócio.',
+    mission: 'Facilitar conexões entre compradores e vendedores, promovendo transações seguras, rápidas e descomplicadas, enquanto ampliamos as oportunidades de negócio para todos os nossos usuários.',
+    language: 'WordPress,Elementor,Php,html,css,API.',
+    images: 'drop.png',
+    link: 'https://classificadosmedicosbrasil.com.br/'
   }
  
 
